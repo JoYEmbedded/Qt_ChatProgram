b0314b7e37b1c29f40a4ab35c6da11e218e9213d
JoYEmbedded <1208687742@qq.com>
2024-07-25 15:15:52 +0800

    basic version without communication

---
+++ mainwidget.cpp
@@ -0,0 +1,129 @@
+#include "mainwidget.h"
+#include "ui_mainwidget.h"
+
+MainWidget::MainWidget(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::MainWidget)
+{
+    ui->setupUi(this);
+    fontsizeInit();
+
+}
+
+MainWidget::~MainWidget()
+{
+    delete ui;
+}
+
+void MainWidget::paintEvent(QPaintEvent *event)
+{
+    QPainter *myPainter = new QPainter(this);
+    myPainter->drawPixmap(rect(),QPixmap(":/new/prefix1/images/bg.jpg"));
+    myPainter->end();
+}
+
+void MainWidget::on_fontComboBox_currentFontChanged(const QFont &f)
+{
+    QFont currentFont = ui->fontComboBox->currentFont();
+    ui->textEdit_Input->setFont(currentFont);
+    ui->textBrowser->setFont(currentFont);
+
+    //防止改字号后变字体导致字号又缩小的情况
+    QFont textFont = ui->textEdit_Input->font();
+    QFont textbrowserFont = ui->textBrowser->font();
+    textFont.setPointSize(ui->comboBox_Fontsize->currentData().toInt());
+    textbrowserFont.setPointSize(ui->comboBox_Fontsize->currentData().toInt());
+    ui->textEdit_Input->setFont(textFont);
+    ui->textBrowser->setFont(textbrowserFont);
+}
+
+void MainWidget::fontsizeInit()
+{
+    int minSize = 8, maxSize = 72;
+    for (int size = minSize; size <= maxSize; size++)
+    {
+        //第二项为关联数据，与currentData对应
+        ui->comboBox_Fontsize->addItem(QString::number(size),size);
+    }
+
+}
+void MainWidget::shortcutInit()
+{
+    QShortcut *exit_shortcut = new QShortcut(QKeySequence("ESC"),this);
+    connect(exit_shortcut,&QShortcut::activated,this,&MainWidget::on_pushButton_exit_clicked);
+
+    QShortcut *emit_shortcut = new QShortcut(QKeySequence("Enter"),this);
+    connect(emit_shortcut,&QShortcut::activated,this,&MainWidget::on_pushButton_emit_clicked);
+}
+void MainWidget::on_comboBox_Fontsize_activated(int index)
+{
+    QFont textFont = ui->textEdit_Input->font();
+    QFont textbrowserFont = ui->textBrowser->font();
+    textFont.setPointSize(ui->comboBox_Fontsize->currentData().toInt());
+    textbrowserFont.setPointSize(ui->comboBox_Fontsize->currentData().toInt());
+    ui->textEdit_Input->setFont(textFont);
+    ui->textBrowser->setFont(textbrowserFont);
+}
+
+
+void MainWidget::on_pushButton_Bold_clicked()
+{
+    if (!ui->textEdit_Input->currentFont().bold())
+    {
+        ui->textEdit_Input->setFontWeight(QFont::Bold);
+    }
+    else
+    {
+        ui->textEdit_Input->setFontWeight(QFont::Normal);
+    }
+
+
+}
+
+
+void MainWidget::on_pushButton_Italic_clicked()
+{
+    ui->textEdit_Input->setFontItalic(!ui->textEdit_Input->currentFont().italic());
+}
+
+
+void MainWidget::on_pushButton_Underline_clicked()
+{
+    ui->textEdit_Input->setFontUnderline(!ui->textEdit_Input->currentFont().underline());
+}
+
+
+void MainWidget::on_pushButton_Color_clicked()
+{
+    fontColor = QColorDialog::getColor(fontColor,this);
+    if(fontColor.isValid())
+    {
+        ui->textEdit_Input->setTextColor(fontColor);
+        ui->textEdit_Input->setFocus();
+    }
+}
+
+
+void MainWidget::on_pushButton_emit_clicked()
+{
+    if(ui->textEdit_Input->document())
+    {
+        //借助新的textedit生成默认格式的html text string格式
+        QTextEdit *timeTextEdit = new QTextEdit;
+        QString timeContent = QTime::currentTime().toString();
+        timeTextEdit->setText(timeContent);
+        QString timeContentToHtml = timeTextEdit->document()->toHtml();
+
+        QString htmlContent = ui->textEdit_Input->document()->toHtml();
+        chatContent = chatContent + timeContentToHtml + htmlContent;
+        ui->textBrowser->setHtml(chatContent);
+
+    }
+}
+
+
+void MainWidget::on_pushButton_exit_clicked()
+{
+    QApplication::quit();
+}
+
---
+++ mainwidget.h
@@ -0,0 +1,46 @@
+#ifndef MAINWIDGET_H
+#define MAINWIDGET_H
+
+#include <QWidget>
+#include <QPainter>
+#include <QColor>
+#include <QColorDialog>
+#include <QTime>
+#include <QShortcut>
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class MainWidget;
+}
+QT_END_NAMESPACE
+
+class MainWidget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    MainWidget(QWidget *parent = nullptr);
+    ~MainWidget();
+
+private:
+    Ui::MainWidget *ui;
+    QColor fontColor;
+    QString chatContent;
+    //重绘，实现窗口自由缩放
+    void paintEvent(QPaintEvent *event) override;
+    void fontsizeInit();
+    void shortcutInit();
+protected:
+
+private slots:
+    void on_fontComboBox_currentFontChanged(const QFont &f);
+    void on_comboBox_Fontsize_activated(int index);
+
+    //实现选中文字或接下来打的文字加减粗
+    void on_pushButton_Bold_clicked();
+    void on_pushButton_Italic_clicked();
+    void on_pushButton_Underline_clicked();
+    void on_pushButton_Color_clicked();
+    void on_pushButton_emit_clicked();
+    void on_pushButton_exit_clicked();
+};
+#endif // MAINWIDGET_H
